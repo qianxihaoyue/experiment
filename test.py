348116ae5ac317ef92a54850ac24759f0316e9fe
千禧皓月 <728534126@qq.com>
2024-07-13 13:43:35 +0800

    7.13

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ from utils.metric import calc_dice
 import os
 from pathlib import Path
 from utils.tool import *
+from utils.logger import  *
+from utils.visualizer import *
 @torch.no_grad()
 def test(model,opt, test_loader,test_dataset):
     dice_list=[]
@@ -36,10 +38,16 @@ if __name__ == '__main__':
     opt.result_path = create_run_name(opt.result_path)
     model=Unet()
     model.load_state_dict(torch.load(opt.trained_checkpoint))
-
     model.cuda()
     model.eval()
     test_dataset = CommonDataset(opt.test_root_path, opt.subdir, opt.image_size)
     test_dataloader = DataLoader(test_dataset, batch_size=opt.batch_size, shuffle=False)
+    opt.result_path = os.path.join(os.path.dirname(os.path.dirname(opt.trained_checkpoint)), "test",os.path.basename(opt.trained_checkpoint).split(".")[0])
+    opt.result_path = create_run_name(opt.result_path)
+    logger = Logger(os.path.join(opt.result_path, "test_log.txt")).get_logger()
+    show_args(opt, logger)
+    opt.result_path = os.path.join(opt.result_path, "save")
+    Path(opt.result_path).mkdir(exist_ok=True, parents=True)
+    visualizer = Visualizer()
     metircs=test(model,opt,test_dataloader,test_dataset)
-    print(metircs["mdice"])
+    logger.info(f'mdice:{metircs["mdice"]}')
